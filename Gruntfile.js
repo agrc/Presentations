c0f4888f7d25206d63c616ab8f1ae71a999ba57a
steveoh <sgourley@utah.gov>
2015-03-04 16:31:40 -0700

    add grunt!

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+/* jshint camelcase:false */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            files: [
+                '**/*.html',
+                '**/*.css'
+            ],
+            options: {
+                livereload: true
+            }
+        },
+        connect: {
+            uses_defaults: {}
+        }
+    });
+
+    // Register tasks.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['connect', 'watch']);
+};
