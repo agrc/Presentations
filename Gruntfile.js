2256d17cd33c7a49a8530b12c47e76d41611cf94
Scott Davis <stdavis@utah.gov>
2015-03-09 20:59:29 -0700

    grunt-gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,21 @@ module.exports = function(grunt) {
         },
         connect: {
             uses_defaults: {}
+        },
+        'gh-pages': {
+            options: {
+                base: 'favorite-things'
+            },
+            src: ['**']
         }
     });
 
     // Register tasks.
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-gh-pages');
 
     // Default task.
     grunt.registerTask('default', ['connect', 'watch']);
+    grunt.registerTask('publish', ['gh-pages'])
 };
