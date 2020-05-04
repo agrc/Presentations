bb4c9bc65bc960faf46f1791de22324d9f6ccc32
Scott Davis <stdavis@utah.gov>
2020-05-04 17:25:32 -0600

    feat: make profile more pretty

--- internal/reactgroup/storybook/demo/src/Profile.js
+++ internal/reactgroup/storybook/demo/src/Profile.js
@@ -4,11 +4,15 @@ export default function Profile(props) {
   const name = `${props.firstName} ${props.lastName}`;
 
   return (
-    <div>
-      <p className="font-bold text-lg">{name}</p>
-      <img className="h-32 border rounded-full" src={props.profileImage} alt="profile" />
-      <p>{props.bio}</p>
-      <p>{props.contact}</p>
+    <div className="flex border bg-gray-100 border-blue-400 m-6 w-1/2 rounded-lg">
+      <div className="flex flex-initial justify-center items-center rounded-l-lg flex-col p-2 bg-white border-r border-blue-400">
+        <p className="font-bold text-lg text-gray-700">{name}</p>
+        <img className="h-32 border shadow-md rounded-full" src={props.profileImage} alt="profile" />
+      </div>
+      <div className="flex flex-col self-center ml-5">
+        <p>{props.bio}</p>
+        <p>{props.contact}</p>
+      </div>
     </div>
   );
 }
