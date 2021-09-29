7c3185bf1e44a3d1aabfca864547c701befcde7c
Jake Adams <jdadams@utah.gov>
2021-09-29 16:12:02 -0600

    wip: intro to python course first push

---
+++ UGIC/2021/IntroToPython/utilities.py
@@ -0,0 +1,18 @@
+def create_county_key(name, separator='_'):
+
+    cleaned_name = name.split(separator)[-1].casefold().replace(' ', '').replace('.', '')
+
+    if cleaned_name == 'richland':
+        cleaned_name = 'rich'
+
+    return cleaned_name
+
+print('This is from the script but will run any time the code is imported or run')
+cleaned = create_county_key('UT_SANJUAN')
+print(cleaned)
+
+
+if __name__ == '__main__':
+    print('This is from the script!')
+    cleaned = create_county_key('UT_CACHE')
+    print(cleaned)
