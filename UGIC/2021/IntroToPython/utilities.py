99d6de75c134c6eee78346086a383cf1e1da455d
Jake Adams <jdadams@utah.gov>
2021-10-04 17:57:42 -0600

    wip: so many updates

--- UGIC/2021/IntroToPython/utilities.py
+++ UGIC/2021/IntroToPython/utilities.py
@@ -1,16 +1,20 @@
 def create_county_key(name, separator='_'):
 
-    cleaned_name = name.split(separator)[-1].casefold().replace(' ', '').replace('.', '')
+    cleaned_name = name.split(separator)[-1].casefold().replace(' ',
+                                                                '').replace(
+                                                                    '.', '')
 
     if cleaned_name == 'richland':
         cleaned_name = 'rich'
 
     return cleaned_name
 
-print('This is from the script but will run any time the code is imported or run')
+
+print(
+    'This is from the script but will run any time the code is imported or run'
+)
 cleaned = create_county_key('UT_SANJUAN')
 print(cleaned)
-
 
 if __name__ == '__main__':
     print('This is from the script!')
